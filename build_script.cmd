586bd691710b19fa1f921c5a88d1575757aa5247
Blightningstar <jasonzatre@hotmail.com>
2024-04-04 21:41:22 -0600

    - Modified the .cmd to be able to create a more meaningful name for dist executable instead of main.py, simplifyed the command as a whole

--- build_script.cmd
+++ build_script.cmd
@@ -1,6 +1,8 @@
 @echo off
 
 REM Run PyInstaller command
-call poetry run pyinstaller --hidden-import=scipy.special._cdflib ^
+call poetry run pyinstaller --log-level=DEBUG ^
     --add-data "e7shop_refresher/image_resources/*;image_resources/" ^
+    --add-data ".venv/Lib/site-packages/:." ^
+    --name e7shop_refresher ^
     e7shop_refresher/main.py
